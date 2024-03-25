8263fc86f3f5f9bea59e3f8bc045cdadbfc1c67c
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-03-25 09:20:46 +0100

    Updated token to pull secret from application.properties

--- src/main/java/ntnu/fullstacksuperteam/backend/service/TokenService.java
+++ src/main/java/ntnu/fullstacksuperteam/backend/service/TokenService.java
@@ -1,7 +1,12 @@
 package ntnu.fullstacksuperteam.backend.service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -9,17 +14,30 @@ import java.time.Instant;
 
 @Service
 public class TokenService {
-    public static final String keyStr = "testsecrettestsecrettestsecrettestsecrettestsecret";
+    @Value("${jwt.secret}")
+    public String keyStr;
     private static final Duration JWT_TOKEN_VALIDITY = Duration.ofMinutes(5);
+    private final Logger logger = LoggerFactory.getLogger(TokenService.class);
 
     public String generateToken(final Long userId) {
         final Instant now = Instant.now();
         final Algorithm hmac512 = Algorithm.HMAC512(keyStr);
         return JWT.create()
                 .withSubject(userId.toString())
-                .withIssuer("exercise5_token_issuer_app")
+                .withIssuer("fst_token_issuer_app") // fst - fullstack super team
                 .withIssuedAt(now)
                 .withExpiresAt(now.plusMillis(JWT_TOKEN_VALIDITY.toMillis()))
                 .sign(hmac512);
+    }
+
+    public String verifyTokenAndGetUserId(final String token) {
+        try {
+            final Algorithm hmac512 = Algorithm.HMAC512(keyStr);
+            final JWTVerifier verifier = JWT.require(hmac512).build();
+            return verifier.verify(token).getSubject();
+        } catch (final JWTVerificationException verificationException) {
+            logger.warn("Token is invalid: {}", verificationException.getMessage());
+            return null;
+        }
     }
 }
