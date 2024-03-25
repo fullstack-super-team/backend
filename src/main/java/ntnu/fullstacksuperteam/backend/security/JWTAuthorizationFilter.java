79d9cac44ee4dba464466ee56295356aec591f1c
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-03-25 09:20:46 +0100

    Updated token to pull secret from application.properties

--- src/main/java/ntnu/fullstacksuperteam/backend/security/JWTAuthorizationFilter.java
+++ src/main/java/ntnu/fullstacksuperteam/backend/security/JWTAuthorizationFilter.java
@@ -11,19 +11,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Service
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
     private final Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.info("Request URI is: {}", request.getRequestURI());
         // check Bearer auth header
         final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (header == null || !header.startsWith("Bearer ")) {
@@ -35,7 +39,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         // Note that we have added userId as subject to the token when it is generated
         // Note also that the token comes in this format 'Bearer token'
         String token = header.substring(7);
-        final String userId = verifyTokenAndGetUserId(token);
+        final String userId = tokenService.verifyTokenAndGetUserId(token);
         if (userId == null) {
             // validation failed or token expired
             filterChain.doFilter(request, response);
@@ -53,16 +57,5 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
         // then, continue with authenticated user context
         filterChain.doFilter(request, response);
-    }
-
-    String verifyTokenAndGetUserId(final String token) {
-        try {
-            final Algorithm hmac512 = Algorithm.HMAC512(TokenService.keyStr);
-            final JWTVerifier verifier = JWT.require(hmac512).build();
-            return verifier.verify(token).getSubject();
-        } catch (final JWTVerificationException verificationException) {
-            logger.warn("Token is invalid: {}", verificationException.getMessage());
-            return null;
-        }
     }
 }
