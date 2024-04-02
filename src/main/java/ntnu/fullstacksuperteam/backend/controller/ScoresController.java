6a72d644c6401f703fd9f98c9b6b9ea6f903adfb
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-04-02 15:43:52 +0200

    feat: Add ScoresController and ScoresService

---
+++ src/main/java/ntnu/fullstacksuperteam/backend/controller/ScoresController.java
@@ -0,0 +1,36 @@
+package ntnu.fullstacksuperteam.backend.controller;
+
+import ntnu.fullstacksuperteam.backend.model.Score;
+import ntnu.fullstacksuperteam.backend.service.ScoresService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/quizzes/{quizId}/scores")
+public class ScoresController {
+    @Autowired
+    private ScoresService scoresService;
+
+    private final Logger logger = LoggerFactory.getLogger(ScoresController.class);
+
+    @GetMapping
+    public ResponseEntity<?> getScoresByQuizId(@PathVariable long quizId) {
+        try {
+            List<Score> scores = new ArrayList<>();
+            return ResponseEntity.status(HttpStatus.OK).body(scores);
+        } catch (Exception exception) {
+            logger.error(exception.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+        }
+    }
+}
---
+++ src/main/java/ntnu/fullstacksuperteam/backend/service/ScoresService.java
@@ -0,0 +1,7 @@
+package ntnu.fullstacksuperteam.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScoresService {
+}
