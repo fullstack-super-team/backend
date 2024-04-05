5756e835240b84a37975c65bd873984646fb8c1f
Vemund Ellingsson Røe <32141244+vemunderoe@users.noreply.github.com>
2024-04-05 09:41:45 +0200

    refactor: Remove unnecessary imports

--- src/main/java/ntnu/fullstacksuperteam/backend/model/Quiz.java
+++ src/main/java/ntnu/fullstacksuperteam/backend/model/Quiz.java
@@ -1,8 +1,6 @@
 package ntnu.fullstacksuperteam.backend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import ntnu.fullstacksuperteam.backend.dto.CreateQuizDTO;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
